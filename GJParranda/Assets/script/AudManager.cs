a242d25e9eb875096cc98261baf71cc74b77af01
Pandaner98 <44508269+Pandaner98@users.noreply.github.com>
2022-07-29 23:02:29 -0400

    push aud changes

---
+++ GJParranda/Assets/script/AudManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] AudioSource guiro;
+    [SerializeField] AudioSource requinto;
+    [SerializeField] AudioSource seguidor;
+    [SerializeField] AudioSource puntador;
+
+
+    [Range(0f,100f)]
+    [SerializeField] float GuiroVolume;
+    [Range(0f, 100f)]
+    [SerializeField] float requintoVolume;
+    [Range(0f, 100f)]
+    [SerializeField] float seguidorVolume;
+    [Range(0f, 100f)]
+    [SerializeField] float puntadorVolume;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        guiro.volume = GuiroVolume / 100;
+        requinto.volume = requintoVolume / 100;
+        seguidor.volume = seguidorVolume / 100;
+        puntador.volume = puntadorVolume / 100;
+    }
+}
