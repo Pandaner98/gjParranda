36dd4c8186c40ed1d1760cc8ca7a673027065198
ABERRIOS002 <40039206+ABERRIOS002@users.noreply.github.com>
2022-07-31 00:51:27 -0400

    Added Menus
    Added a Main Menu scene, as well as the Win and Loss screens.

---
+++ GJParranda/Assets/script/QuitGame.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuitGame : MonoBehaviour
+{
+
+    private void OnApplicationQuit()
+    {
+        Application.Quit();
+    }
+}
